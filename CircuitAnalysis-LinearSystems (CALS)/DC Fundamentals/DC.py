1a12e8843bb93195e83f17c21f206a7f7c2e7586
Donald Jackson <103725783+ReavesX@users.noreply.github.com>
2025-07-17 22:01:33 -0400

    update

---
+++ CircuitAnalysis-LinearSystems (CALS)/DC Fundamentals/DC.py
@@ -0,0 +1,36 @@
+import hpprime as h
+import MENU as m
+import ohmslaw as ohmslaw
+
+def DC_Main():
+    while True: 
+        # Show button-based menu
+        choice = m.create_button_menu("ΩLaw/Combos", "Simplify", "W or J"," "," ")
+
+        if choice == 0:
+            ohmslaw.ohmslaw_main()
+        elif choice == 1:
+            m.screenClear()
+            h.eval('print("Not implemented yet.")')
+            h.eval('wait(2)')
+            m.screenClear()
+        elif choice == 2:
+            m.screenClear()
+            h.eval('print("Not implemented yet.")')
+            h.eval('wait(2)')
+            m.screenClear()
+        elif choice == 3:
+            m.screenClear()
+            h.eval('print("Not implemented yet.")')
+            h.eval('wait(2)')
+            m.screenClear()
+        elif choice == 4 or choice == 5 or choice == 6:  # Exit
+            m.screenClear()
+            h.eval('print("Goodbye!")')
+            break
+        else:
+            # Soft key press that wasn't mapped
+            m.screenClear()
+            h.eval('print("Invalid selection.")')
+            h.eval('wait(2)')
+            m.screenClear()
